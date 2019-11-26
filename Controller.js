7b65d88a9d2e4da266932c50f8d1e0c23235f10e
RomanCebada <47892127+RomanCebada@users.noreply.github.com>
2019-11-25 19:21:42 -0600

    Add files via upload

--- Controller.js
+++ Controller.js
@@ -2,12 +2,12 @@ document.getElementById('res').addEventListener('click',()=>{
     if(document.getElementById('est').checked){
         var ini = document.getElementById('est').value;
     }
-    var P11 = document.getElementById('P11').value;
-    var P12 = document.getElementById('P12').value;
-    var P22 = document.getElementById('P22').value;
-    var P21 = document.getElementById('P21').value;
-    var cant = document.getElementById('cant').value;
+    var P11 = parseFloat(document.getElementById('P11').value);
+    var P12 = parseFloat(document.getElementById('P12').value);
+    var P22 = parseFloat(document.getElementById('P22').value);
+    var P21 = parseFloat(document.getElementById('P21').value);
+    var cant = parseInt(document.getElementById('cant').value);
 
     const markov = new Markov();
     console.log(markov.resolver(ini,P11,P12,P22,P21,cant));
-})
+});
--- Markov.js
+++ Markov.js
@@ -3,17 +3,18 @@ class Markov{
 
     }
     resolver(ini,P11,P12,P22,P21,cant) {
+        console.log(ini,P11,P12,P21,P22,cant); 
         if(ini == 'S1'){
             var PS1,PS2;
             for(let i=0;i<cant;i++){
                 if(i==0){
-                    let temp1 = Math.pow(P11,2)+(P12)(P21);
+                    let temp1 = Math.pow(P11,2)+(P12*P21);
                     let temp2 = 1 - temp1;
                     PS1 = temp1;
                     PS2 = temp2;
                 }
                 else{
-                    let temp1 = (PS1)(P11)+(PS2)(P21);
+                    let temp1 = (PS1*P11)+(PS2*P21);
                     let temp2 = 1 - temp1;
                     PS1 = temp1;
                     PS2 = temp2;
@@ -24,13 +25,13 @@ class Markov{
             var PS1,PS2;
             for(let i=0;i<cant;i++){
                 if(i==0){
-                    let temp1 = Math.pow(P22,2)+(P21)(P12);
+                    let temp1 = Math.pow(P22,2)+(P21*P12);
                     let temp2 = 1 - temp1;
                     PS1 = temp1;
                     PS2 = temp2;
                 }
                 else{
-                    let temp1 = (PS1)(P22)+(PS2)(P12);
+                    let temp1 = (PS1*P22)+(PS2*P12);
                     let temp2 = 1 - temp1;
                     PS1 = temp1;
                     PS2 = temp2;
