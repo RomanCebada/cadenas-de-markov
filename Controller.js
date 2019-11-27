e40ba178f45938c1bc1abd0f57ce618c95619a45
RomanCebada <47892127+RomanCebada@users.noreply.github.com>
2019-11-26 20:39:51 -0600

    Add files via upload

--- Controller.js
+++ Controller.js
@@ -9,5 +9,19 @@ document.getElementById('res').addEventListener('click',()=>{
     var cant = parseInt(document.getElementById('cant').value);
 
     const markov = new Markov();
-    console.log(markov.resolver(ini,P11,P12,P22,P21,cant));
+    var resultadosFinales = markov.resolver(ini,P11,P12,P22,P21,cant);
+    var final = resultadosFinales.split(' ');
+    if(ini=='S1'){
+        document.getElementById('success1').innerHTML = `<div class="bg-success rounded py-5">La probabilidad de S1 es de: ${(final[0]*100).toFixed(3)}% en base a los datos ingresados anteriormente</div>`;
+    }
+    else{
+        document.getElementById('success1').innerHTML = `<div class="bg-success rounded py-5">La probabilidad de S2 es de: ${(final[1]*100).toFixed(3)}% en base a los datos ingresados anteriormente</div>`;
+    }
+
+    document.getElementById('P11').value ="";
+    document.getElementById('P12').value ="";
+    document.getElementById('P22').value ="";
+    document.getElementById('P21').value ="";
+    document.getElementById('cant').value ="";
+    document.getElementById('est').checked=false;
 });
